fedf5b5b0f8e5cd4e2df4c9cfb06a28c8785c00d
Andones <switalski.hubert@gmail.com>
2013-05-31 20:06:58 +0200

    syfki do obserwowania zmian pliku

---
+++ Server/filesystemwatcher.h
@@ -0,0 +1,19 @@
+#ifndef FILESYSTEMWATCHER_H
+#define FILESYSTEMWATCHER_H
+
+#include <QFileSystemWatcher>
+#include <QObject>
+
+class FileSystemWatcher : public QObject
+{
+    Q_OBJECT
+public:
+    FileSystemWatcher(QString path, QObject* parent = 0);
+private slots:
+    void fileChangedSlot(QString path);
+    void dirChangedSlot(QString path);
+private:
+    QFileSystemWatcher *watcher;
+};
+
+#endif // FILESYSTEMWATCHER_H
